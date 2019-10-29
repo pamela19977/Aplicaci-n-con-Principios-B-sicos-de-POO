bb6e597bde1c4859fb95478a9bab8f18823cc9ba
pamela1997 <pamela_becerril_97@hotmail.com>
2019-10-28 23:43:57 -0600

    primer commit

--- src/java/CtrlForm.java
+++ src/java/CtrlForm.java
@@ -1,35 +1,39 @@
-
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-
 @Named(value = "CtrlForm")
 @RequestScoped
 public class CtrlForm {
-  private String nombre;
-  private String grupo;
-  
-  public String getNombre() {
-    return nombre;
-  }
- 
-  public void setNombre(String nombre) {
-    this.nombre = nombre; 
-  }
-  
-  public String getGrupo(){
-    return grupo;  
-  }
-  
-  public void setGrupo(String grupo) {
-      this.grupo = grupo;
-  }
-  
-  public void saluda() {
-    final String saludo = "Hola" + getNombre() + "Tu grupo es:" + getGrupo() + ".";
-    FacesContext.getCurrentInstance()
-        .addMessage(null, new FacesMessage(saludo));
-  }
+    private String nombre;
+    private int orden;
+    private int total; 
+
+    public String getNombre() {
+      return nombre;
+    }
+
+    public void setNombre(String nombre) {
+      this.nombre = nombre; 
+    }
+
+    public int getOrden(){
+      return orden;  
+    }
+
+    public void setOrden(int orden) {
+        this.orden = orden;
+    }
+    
+    public int calculo(){
+        total= orden * 25;
+        return total; 
+    }
+
+    public void saludo() {
+      final String respuesta = "Hola   " + getNombre() + " tienes que pagar $  " + calculo()  + ".";
+      FacesContext.getCurrentInstance()
+          .addMessage(null, new FacesMessage(respuesta));
+    }
 }
